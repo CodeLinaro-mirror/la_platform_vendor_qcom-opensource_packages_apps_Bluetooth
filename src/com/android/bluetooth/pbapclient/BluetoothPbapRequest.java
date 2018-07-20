e596fb2499167580dc92769420e392e6b85cefd6
Bandari Ramesh <quic_rbandari@quicinc.com>
2018-07-20 08:18:05 +0800

    PBAP_Client: PBAP_Client All changes
    PBAP_Client: Support optional features
    
    1. PullPhoneBook, including PullPhoneBook
    
    2. SetPhoneBook, including SetPath ROOT/Up/Down
    
    3. PullvCardListing
    
    4. PullvCardEntry
    
    5. Abort
    
    (cherry-picked from commit Ie59623dbd607608087d272aaa1644fd284b40b53)
    
    change-Id: Icfb94b411b465c26da3cb091b4b77f33ea214489
    
    Bluetooth-PBAP: Import package Uri and ContactsContract
    
    (cherry-picked from commit Iaa0d1aea1bff29a7cf016d0e8e8ca52532b6fb2e)
    
    change-Id: I4aaedeadd34cd39b22f81c34ce33198089b3de16
    
    PBAP_Client: Support to store call log for multiple devices
    
    (cherry-picked from commit I09e38c3e3e48c990446426917a7b57a1e53eebe0)
    
    change-Id: I4ed8a34b5f91af2f5a349d77bc9b0726916eedf5
    
    PBAP_CLIENT: Add null pointer check in updateTimesContacted()
    
    IllegalArgumentException will be occured if the call log vcard
    format is like the following:
    TEL;TYPE=0:null
    
    This code change is to add null pointer check to protect the
    use of key.
    
    (cherry-picked from commit Ic6f6b45297c4ce3f0c26027226ef1cbb3c6e76a6)
    
    change-Id: I8aea19e5980e1b7d1808c950d6350c00841388fa
    
    PBAP_Client: Fix compilation issues
    
    Change-Id: Icd87c36d4d91932e9f096bb0e5ff01dec9f264f9
    
    PBAP_CLIENT: Process ACL disconnection while in connecting state
    
    change-Id: I38b9a93ca740a9ce6e4117f349faa9f71ff595e9
    
    PCE : Updated the get connection policy check
    
    change-Id: I1a949f0a82189467f695c9178af201d0945e9a6d
    
    PCE : Added parameters for call log downloading
    
    Parameters are Maxlist, offset count and vcard format.
    
    change-Id: I94fe0647f83fb19f97c2014899033dbac67660f8

--- src/com/android/bluetooth/pbapclient/BluetoothPbapRequest.java
+++ src/com/android/bluetooth/pbapclient/BluetoothPbapRequest.java
@@ -12,6 +12,10 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * Changes from Qualcomm Innovation Center, Inc. are provided under the following license:
+ * Copyright (c) 2024 Qualcomm Innovation Center, Inc. All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause-Clear
  */
 
 package com.android.bluetooth.pbapclient;
@@ -46,7 +50,7 @@ abstract class BluetoothPbapRequest {
 
     protected int mResponseCode;
 
-    private boolean mAborted = false;
+    protected boolean mAborted = false;
 
     private ClientOperation mOp = null;
 
@@ -56,6 +60,10 @@ abstract class BluetoothPbapRequest {
 
     public final boolean isSuccess() {
         return (mResponseCode == ResponseCodes.OBEX_HTTP_OK);
+    }
+
+    public int getResponseCode() {
+        return mResponseCode;
     }
 
     public void execute(ClientSession session) throws IOException {
---
+++ src/com/android/bluetooth/pbapclient/BluetoothPbapRequestPullVcardEntry.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright (c) 2018, The Linux Foundation. All rights reserved.
+ * Not a contribution
+ *
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.bluetooth.pbapclient;
+
+import android.accounts.Account;
+import android.util.Log;
+
+import com.android.vcard.VCardEntry;
+import com.android.bluetooth.pbapclient.ObexAppParameters;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+
+import com.android.obex.HeaderSet;
+
+final class BluetoothPbapRequestPullVcardEntry extends BluetoothPbapRequest {
+
+    private static final boolean VDBG = true;
+
+    private static final String TAG = "BluetoothPbapRequestPullVcardEntry";
+
+    private static final String TYPE = "x-bt/vcard";
+
+    private BluetoothPbapVcardList mResponse;
+
+    private Account mAccount;
+
+    private final byte mFormat;
+
+    public BluetoothPbapRequestPullVcardEntry(
+            String pbName, Account account, long filter, byte format) {
+        mAccount = account;
+
+        mHeaderSet.setHeader(HeaderSet.NAME, pbName);
+
+        mHeaderSet.setHeader(HeaderSet.TYPE, TYPE);
+
+        ObexAppParameters oap = new ObexAppParameters();
+
+        /* make sure format is one of allowed values */
+        if ((format != PbapClientConnectionHandler.VCARD_TYPE_21) &&
+            (format != PbapClientConnectionHandler.VCARD_TYPE_30)) {
+            format = PbapClientConnectionHandler.VCARD_TYPE_21;
+        }
+
+        if (filter != 0) {
+            oap.add(OAP_TAGID_FILTER, filter);
+        }
+
+        oap.add(OAP_TAGID_FORMAT, format);
+
+        oap.addToHeaderSet(mHeaderSet);
+
+        mFormat = format;
+    }
+
+    @Override
+    protected void readResponse(InputStream stream) throws IOException {
+        Log.v(TAG, "readResponse");
+
+        mResponse = new BluetoothPbapVcardList(mAccount, stream, mFormat);
+        if (VDBG) {
+            Log.d(TAG, "Read " + mResponse.getCount() + " entries.");
+        }
+    }
+
+    public int getCount() {
+        return mResponse.getCount();
+    }
+
+    public ArrayList<VCardEntry> getList() {
+        return mResponse.getList();
+    }
+
+    public VCardEntry getFirst() {
+        return mResponse.getFirst();
+    }
+
+    public String getVcard() {
+        VCardEntry vcard = getFirst();
+        PhonebookEntry pb = new PhonebookEntry(vcard);
+        return pb.toString();
+    }
+}
