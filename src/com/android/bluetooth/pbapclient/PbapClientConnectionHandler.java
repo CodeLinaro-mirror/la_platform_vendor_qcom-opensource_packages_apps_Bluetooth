46378331d0900ea2c9eabf0a836d414b0b0d4225
Andrew Cheng <chengca@google.com>
2019-10-10 15:19:46 -0700

    NullPointerException handling, connectObexSession
    In connectObexSession, if a null mSocket is passed to
    BluetoothObexTransport, then a NullPointerException will be thrown when
    a new ClientSession is created.
    
    This can occur if abort() --> closeSocket() is called between the calls
    to connectSocket() and connectObexSession().
    
    Bug: 142056980
    Test: cycle Bluetooth adapter on/off 1k times
    Change-Id: I78691eb03cd4f3e12922a31b1bc4f4144353c125
    (cherry picked from commit 4a3a2fed646019f590b39050a214cfbfc101fae1)

--- src/com/android/bluetooth/pbapclient/PbapClientConnectionHandler.java
+++ src/com/android/bluetooth/pbapclient/PbapClientConnectionHandler.java
@@ -336,10 +336,15 @@ class PbapClientConnectionHandler extends Handler {
             }
             HeaderSet connectionResponse = mObexSession.connect(connectionRequest);
 
-            connectionSuccessful = (connectionResponse.getResponseCode() ==
-                    ResponseCodes.OBEX_HTTP_OK);
-            if (DBG) Log.d(TAG, "Success = " + Boolean.toString(connectionSuccessful));
-        } catch (IOException e) {
+            connectionSuccessful =
+                    (connectionResponse.getResponseCode() == ResponseCodes.OBEX_HTTP_OK);
+            if (DBG) {
+                Log.d(TAG, "Success = " + Boolean.toString(connectionSuccessful));
+            }
+        } catch (IOException | NullPointerException e) {
+            // Will get NPE if a null mSocket is passed to BluetoothObexTransport.
+            // mSocket can be set to null if an abort() --> closeSocket() was called between
+            // the calls to connectSocket() and connectObexSession().
             Log.w(TAG, "CONNECT Failure " + e.toString());
             closeSocket();
         }
