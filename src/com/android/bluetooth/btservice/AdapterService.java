7f40dfbd6c3479b097a83f9767f781c652612502
weichinweng <weichinweng@google.com>
2020-04-24 14:54:36 +0800

    NIAP: Implement BluetoothKeystore feature
    Bug: 148758680
    Test: atest BluetoothKeystoreServiceTest
    Change-Id: I19ec7b4ece5183c3d401844a089a287a37d324c5
    Merged-In: I19ec7b4ece5183c3d401844a089a287a37d324c5
    (cherry picked from commit 1ac6f03bcc46b82184e736899316b5da14935aa8)

--- src/com/android/bluetooth/btservice/AdapterService.java
+++ src/com/android/bluetooth/btservice/AdapterService.java
@@ -112,6 +112,7 @@ import com.android.bluetooth.Utils;
 import com.android.bluetooth.a2dp.A2dpService;
 import com.android.bluetooth.a2dpsink.A2dpSinkService;
 import com.android.bluetooth.btservice.RemoteDevices.DeviceProperties;
+import com.android.bluetooth.btservice.bluetoothkeystore.BluetoothKeystoreService;
 import com.android.bluetooth.btservice.storage.DatabaseManager;
 import com.android.bluetooth.btservice.storage.MetadataDatabase;
 import com.android.bluetooth.gatt.GattService;
@@ -267,6 +268,7 @@ public class AdapterService extends Service {
     private AppOpsManager mAppOps;
     private VendorSocket mVendorSocket;
 
+    private BluetoothKeystoreService mBluetoothKeystoreService;
     private A2dpService mA2dpService;
     private A2dpSinkService mA2dpSinkService;
     private HeadsetService mHeadsetService;
@@ -566,6 +568,8 @@ public class AdapterService extends Service {
         mAdapterStateMachine =  AdapterState.make(this);
         mJniCallbacks = new JniCallbacks(this, mAdapterProperties);
         mVendorSocket = new VendorSocket(this);
+        mBluetoothKeystoreService = new BluetoothKeystoreService(isNiapMode());
+        mBluetoothKeystoreService.start();
         initNative(isGuest(), isNiapMode());
         mNativeAvailable = true;
         mCallbacks = new RemoteCallbackList<IBluetoothCallback>();
@@ -953,6 +957,10 @@ public class AdapterService extends Service {
         if (mSdpManager != null) {
             mSdpManager.cleanup();
             mSdpManager = null;
+        }
+
+        if (mBluetoothKeystoreService != null) {
+            mBluetoothKeystoreService.cleanup();
         }
 
         if (mNativeAvailable) {
@@ -2149,6 +2157,9 @@ public class AdapterService extends Service {
                 service.onBrEdrDown();
             } else {
                 service.disable();
+            }
+            if (service.mBluetoothKeystoreService != null) {
+                service.mBluetoothKeystoreService.factoryReset();
             }
             return service.factoryReset();
         }
