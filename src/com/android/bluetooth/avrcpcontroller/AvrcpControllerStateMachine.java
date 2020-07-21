3b78394b5ee76b93942a6b823a06f38bd3209578
Yanfei Zhou <zyanfei@codeaurora.org>
2020-07-21 17:13:00 +0800

    AVRCP_CT: Fix MiniPlaybackControls is missing after resumed from LPM
    Description:
    MiniPlaybackControls is gone when resumed from low power mode(LPM)
    
    Reason:
    Default state for newly cretead addressed player is
    PlaybackState#STATE_NONE. This results into the visibility of
    MiniPlaybackControls is set to false.
    
    Solution:
    Retrieve playback state when new addressed player is created.
    
    Change-Id: Ifd01a1bc464e3d6d65314508f2b9e1274099f559

--- src/com/android/bluetooth/avrcpcontroller/AvrcpControllerStateMachine.java
+++ src/com/android/bluetooth/avrcpcontroller/AvrcpControllerStateMachine.java
@@ -961,6 +961,8 @@ class AvrcpControllerStateMachine extends StateMachine {
                         BluetoothMediaBrowserService.notifyChanged(mBrowseTree.mRootNode);
                     }
                     removeUnusedArtworkFromBrowseTree();
+                    // Get playback state for new addressed player
+                    mService.getPlaybackStateNative(mDeviceAddress);
                     return true;
 
                 case MESSAGE_PROCESS_SUPPORTED_APPLICATION_SETTINGS:
