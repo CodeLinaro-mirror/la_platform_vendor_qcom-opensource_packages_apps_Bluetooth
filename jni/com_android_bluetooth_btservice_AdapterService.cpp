299c1a528611b1ce3dbd06b7b1f1bf192350441b
Rahul Sabnis <rahulsabnis@google.com>
2020-07-31 19:47:06 -0700

    Pass whether the local device is an atv device to the native layer to determine whether to include pairing dialogs for justworks and encryption only LE pairing
    Tag: #feature
    Bug: 157038281
    Test: Manual
    Merged-In: Ib7575ff3d5f7e0c208743eff652440f7947dfed7
    Change-Id: Ib7575ff3d5f7e0c208743eff652440f7947dfed7
    (cherry picked from commit b11ac32ea3588b6910b58ec285e41bddeee75844)
    (cherry picked from commit 9a0680b02d11e1455c94243808e06d55fa50c1b9)

--- jni/com_android_bluetooth_btservice_AdapterService.cpp
+++ jni/com_android_bluetooth_btservice_AdapterService.cpp
@@ -793,7 +793,7 @@ static void classInitNative(JNIEnv* env, jclass clazz) {
   }
 }
 
-static bool initNative(JNIEnv* env, jobject obj) {
+static bool initNative(JNIEnv* env, jobject obj, jboolean isAtvDevice) {
   ALOGV("%s", __func__);
 
   android_bluetooth_UidTraffic.clazz =
@@ -807,7 +807,7 @@ static bool initNative(JNIEnv* env, jobject obj) {
     return JNI_FALSE;
   }
 
-  int ret = sBluetoothInterface->init(&sBluetoothCallbacks);
+  int ret = sBluetoothInterface->init(&sBluetoothCallbacks, isAtvDevice == JNI_TRUE ? 1 : 0);
   if (ret != BT_STATUS_SUCCESS) {
     ALOGE("Error while setting the callbacks: %d\n", ret);
     sBluetoothInterface = NULL;
@@ -1454,7 +1454,7 @@ static jboolean readLocalOobDataNative(JNIEnv* env, jobject obj) {
 static JNINativeMethod sMethods[] = {
     /* name, signature, funcPtr */
     {"classInitNative", "()V", (void*)classInitNative},
-    {"initNative", "()Z", (void*)initNative},
+    {"initNative", "(Z)Z", (void*)initNative},
     {"cleanupNative", "()V", (void*)cleanupNative},
     {"enableNative", "(Z)Z", (void*)enableNative},
     {"disableNative", "()Z", (void*)disableNative},
