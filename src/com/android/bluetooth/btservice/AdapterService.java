b9eb31e0e806a27c60efab1416e943cd045c3b81
Swathi Sridhar <swathisr@codeaurora.org>
2020-11-09 14:07:12 +0530

    BT: Add property for enabling NIAP from BT App
    Change-Id: Ia81ca8547bfb8543f40491343f6c77aa8558cc01

--- src/com/android/bluetooth/btservice/AdapterService.java
+++ src/com/android/bluetooth/btservice/AdapterService.java
@@ -4219,7 +4219,7 @@ public class AdapterService extends Service {
     }
 
     private boolean isNiapMode() {
-        return Settings.Global.getInt(getContentResolver(), "niap_mode", 0) == 1;
+        return SystemProperties.getBoolean("persist.bluetooth.enable.niap", false); //Settings.Global.getInt(getContentResolver(), "niap_mode", 0) == 1;
     }
 
     /**
