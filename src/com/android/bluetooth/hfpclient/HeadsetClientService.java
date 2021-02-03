94d2a741de0970c76d0fe9ffca4e28816f3fd0d5
himta ram <hram@codeaurora.org>
2021-02-03 10:21:35 +0530

    BT: hfpclient: delay the SCO connection from hfp client side
      Delay the SCO connection by 5 sec from hfp client side so that
      HFP AG can intiate the SCO connection.
      Upon successfull SCO connection, hfp client state will be moved to
      AudioOn and delayed SCO connection from hfp client side will be
      gracefully handled.
      Upon unsuccessfull SCO connection, delayed SCO connection from
      hfp client side will be proccessed in hfp client's connected state.
    
    Change-Id: I775fed1fd5a7cf40d54db06cd96c81fd443a432d

--- src/com/android/bluetooth/hfpclient/HeadsetClientService.java
+++ src/com/android/bluetooth/hfpclient/HeadsetClientService.java
@@ -68,6 +68,7 @@ public class HeadsetClientService extends ProfileService {
     // Maxinum number of devices we can try connecting to in one session
     private static final int MAX_STATE_MACHINES_POSSIBLE = 100;
     private static final int MAX_HFP_CLIENTS_SUPPORTED = 1;
+    private static final int CONNECT_AUDIO_DELAY = 5000;
     public static final String HFP_CLIENT_STOP_TAG = "hfp_client_stop_tag";
 
     @Override
@@ -710,7 +711,11 @@ public class HeadsetClientService extends ProfileService {
         if (sm.isAudioOn()) {
             return false;
         }
-        sm.sendMessage(HeadsetClientStateMachine.CONNECT_AUDIO);
+        /* Sending message with a delay, In most cases SCO will be
+         * initiated by AG, In case its not done till 5 sec, DUT
+         * ( HFP-Client ) will send SCO request from here
+         */
+        sm.sendMessageDelayed(HeadsetClientStateMachine.CONNECT_AUDIO, CONNECT_AUDIO_DELAY);
         return true;
     }
 
