10538059103e87e959aad7cf20eb8d9cf96215c0
himta ram <hram@codeaurora.org>
2021-02-18 16:39:38 +0530

    hfpclient: remove the connect audio message when StateMachine moved to AudioOn
    HFPClient is delaying the SCO connection by 5 sec by
    that time HFP AG can intiate the SCO connection.
    Remove the connect audio message from the StateMachine message queue
    when StateMachine moved to AudioOn. This will avoid the SCO
    connection trigger from hfp client in case of call ends before 5 sec.
    
    Change-Id: I1a11ae8bc80505a4801b9e3261a2e72c8f1d30f2

--- src/com/android/bluetooth/hfpclient/HeadsetClientStateMachine.java
+++ src/com/android/bluetooth/hfpclient/HeadsetClientStateMachine.java
@@ -1748,6 +1748,7 @@ public class HeadsetClientStateMachine extends StateMachine {
             if (DBG) {
                 Log.d(TAG, "Enter AudioOn: " + getCurrentMessage().what);
             }
+            removeMessages(CONNECT_AUDIO);
             broadcastAudioState(mCurrentDevice, BluetoothHeadsetClient.STATE_AUDIO_CONNECTED,
                     BluetoothHeadsetClient.STATE_AUDIO_CONNECTING);
         }
