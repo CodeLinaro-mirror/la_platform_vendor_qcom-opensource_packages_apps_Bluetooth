54c0e9cb64ab4359c4eb1fbbc180761bd8bb9172
Srilakshmi Kanagalla <kanagall@codeaurora.org>
2021-02-26 11:32:10 +0530

    Audio connect changes to pass pts tests ATAH/BV-01-1, ORR/BV-02-1
    After making and flashing build with the change,
    1. adb root && adb wait-for-device && adb remount
    2. adb shell am broadcast --ei "android.bluetooth.extra.audio.STATE" <audio ind value> -a "android.bluetooth.action.HFP_CLIENT_AUDIO_ACTION"
    1-	connect audio
    0-	disconnect audio.
    
    Change-Id: Iba772522f140cef75c9792a10d37f03f12b1162d

--- src/com/android/bluetooth/hfpclient/HeadsetClientService.java
+++ src/com/android/bluetooth/hfpclient/HeadsetClientService.java
@@ -55,6 +55,8 @@ public class HeadsetClientService extends ProfileService {
     private static final boolean DBG = false;
     private static final String TAG = "HeadsetClientService";
 
+    private static final String ACTION_AUDIO_CONN_DISCONN = "android.bluetooth.action.HFP_CLIENT_AUDIO_ACTION";
+    private static final String EXTRA_AUDIO_STATE = "android.bluetooth.extra.audio.STATE";
     private HashMap<BluetoothDevice, HeadsetClientStateMachine> mStateMachineMap = new HashMap<>();
     private static HeadsetClientService sHeadsetClientService;
     private NativeInterface mNativeInterface = null;
@@ -101,6 +103,7 @@ public class HeadsetClientService extends ProfileService {
         mStateMachineMap.clear();
 
         IntentFilter filter = new IntentFilter(AudioManager.VOLUME_CHANGED_ACTION);
+        filter.addAction(ACTION_AUDIO_CONN_DISCONN);
         registerReceiver(mBroadcastReceiver, filter);
 
         // Start the HfpClientConnectionService to create connection with telecom when HFP
@@ -180,6 +183,24 @@ public class HeadsetClientService extends ProfileService {
                                 sm.sendMessage(HeadsetClientStateMachine.SET_SPEAKER_VOLUME,
                                         streamValue);
                             }
+                        }
+                    }
+                }
+            } else if (intent.getAction().equals(ACTION_AUDIO_CONN_DISCONN)) {
+                /*Audio connect changes to pass pts tests ATAH/BV-01-1, ORR/BV-02-1 */
+                Log.e(TAG, "HeadsetClientService -  Received ACTION_AUDIO_CONN_DISCONN");
+                int con_status = intent.getIntExtra( EXTRA_AUDIO_STATE, 0);
+                Log.d(TAG, " HeadsetClientService con_status" + con_status);
+                for (HeadsetClientStateMachine sm : mStateMachineMap.values()) {
+                    if (sm != null) {
+                        if(con_status == 1){
+                            Log.d(TAG, " HeadsetClientService AUDIO_CONNECT message to statemachine");
+                            sm.sendMessage(
+                                HeadsetClientStateMachine.CONNECT_AUDIO );
+                        } else {
+                            Log.d(TAG, " HeadsetClientService AUDIO_DISCONNECT message to statemachine");
+                            sm.sendMessage(
+                                HeadsetClientStateMachine.DISCONNECT_AUDIO );
                         }
                     }
                 }
