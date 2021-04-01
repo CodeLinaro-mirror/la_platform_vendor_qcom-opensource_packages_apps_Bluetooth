b97ab9fc4ccfa7f2fb4b4d7f511bf64c625fd6af
Rohon Das <rohodas@codeaurora.org>
2021-04-01 17:23:37 +0530

    Roaming changes to pass PTS test HFP/AG/PSI/BV-02-C
    After making and flashing build with the change,
    1. adb root && adb wait-for-device && adb remount
    2. adb shell am broadcast --ei "android.bluetooth.extra.roaming.STATE" <roaming ind value> -a "android.bluetooth.action.ROAMING_STATE_CHANGED"
    <roaming ind value>
    1-      Roaming
    0-      Home network
    
    Change-Id: Ic0cab736d1fea62d4627241584a608ebf16f8167

--- src/com/android/bluetooth/hfp/HeadsetStateMachine.java
+++ src/com/android/bluetooth/hfp/HeadsetStateMachine.java
@@ -141,7 +141,8 @@ public class HeadsetStateMachine extends StateMachine {
     private static final int PROCESS_CPBR = 105;
 
     private static final int CONNECT_TIMEOUT = 201;
-
+    private static final String EXTRA_ROAMING_STATE = "android.bluetooth.extra.roaming.STATE";
+    static final int UPDATE_ROAMING_STATE = 25;
     private static final int CLCC_RSP_TIMEOUT_MS = 5000;
     private static final int QUERY_PHONE_STATE_CHANGED_DELAYED = 100;
     // NOTE: the value is not "final" - it is modified in the unit tests
@@ -1514,6 +1515,15 @@ public class HeadsetStateMachine extends StateMachine {
                      }
                      break;
                 }
+                case UPDATE_ROAMING_STATE:
+                    Intent intent1 = (Intent) message.obj;
+                    int roaming_status = intent1.getIntExtra(EXTRA_ROAMING_STATE, 0);
+                    //code to send the roaming state to remote
+                    Log.e(TAG, "UPDATE_ROAMING_STATE received " + roaming_status);
+                    HeadsetPhoneState phoneState = mSystemInterface.getHeadsetPhoneState();
+                    HeadsetDeviceState deviceState = new HeadsetDeviceState(phoneState.getCindService(), roaming_status, phoneState.getCindSignal(), phoneState.getCindBatteryCharge());
+                    mNativeInterface.notifyDeviceStatus(mDevice, deviceState);
+                    break;
                 default:
                     return super.processMessage(message);
             }
@@ -1764,6 +1774,15 @@ public class HeadsetStateMachine extends StateMachine {
                      stateLogD("SCO_RETRIIAL_NOT_REQ: ");
                      mIsRetrySco = false;
                      break;
+                case UPDATE_ROAMING_STATE:
+                    Intent intent1 = (Intent) message.obj;
+                    int roaming_status = intent1.getIntExtra(EXTRA_ROAMING_STATE, 0);
+                    //code to send the roaming state to remote
+                    Log.e(TAG, "UPDATE_ROAMING_STATE received " + roaming_status);
+                    HeadsetPhoneState phoneState = mSystemInterface.getHeadsetPhoneState();
+                    HeadsetDeviceState deviceState = new HeadsetDeviceState(phoneState.getCindService(), roaming_status, phoneState.getCindSignal(), phoneState.getCindBatteryCharge());
+                    mNativeInterface.notifyDeviceStatus(mDevice, deviceState);
+                    break;
                 case STACK_EVENT:
                     HeadsetStackEvent event = (HeadsetStackEvent) message.obj;
                     stateLogD("STACK_EVENT: " + event);
@@ -2298,7 +2317,7 @@ public class HeadsetStateMachine extends StateMachine {
                 mNativeInterface.phoneStateChange(mDevice, updateCallState);
                 mIsCallIndDelay = true;
             }
-            
+
             /* The device is blacklisted for sending incoming call setup
              * indicator after SCO disconnection and sending active call end
              * indicator. While the incoming call setup indicator is in queue,
