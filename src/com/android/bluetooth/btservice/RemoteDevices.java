1f19f07e0485e368bc4e1ff8e98659ded684f2a7
janardhana rao bokka <jbokka@codeaurora.org>
2021-04-29 17:34:58 +0530

    GAP : (DUMO + RPA) Handling ACTION_UUID not to send for rand addr
    Discarding ACTION_UUID for rand_addr in the case DUMO + RPA.
    Because for Setting and other profiles, Identity address is the
    primary address.
    
    Change-Id: I919b701a25802a2985c882425f7fb301689d47d3

--- src/com/android/bluetooth/btservice/RemoteDevices.java
+++ src/com/android/bluetooth/btservice/RemoteDevices.java
@@ -555,6 +555,10 @@ final class RemoteDevices {
     }
 
     private void sendUuidIntent(BluetoothDevice device, DeviceProperties prop) {
+        if (sAdapterService.isIgnoreDevice(device)) {
+            debugLog("Ignoring action_UUID " +device.getAddress());
+            return;
+        }
         Intent intent = new Intent(BluetoothDevice.ACTION_UUID);
         intent.putExtra(BluetoothDevice.EXTRA_DEVICE, device);
         intent.putExtra(BluetoothDevice.EXTRA_UUID, prop == null ? null : prop.mUuids);
@@ -701,6 +705,10 @@ final class RemoteDevices {
                                 debugLog("Skip name update for " + bdDevice);
                                 break;
                             }
+                            if(sAdapterService.isIgnoreDevice(bdDevice)) {
+                                debugLog("Skip name update for Duplicate Addr" + bdDevice);
+                                break;
+                            }
                             device.mName = newName;
                             intent = new Intent(BluetoothDevice.ACTION_NAME_CHANGED);
                             intent.putExtra(BluetoothDevice.EXTRA_DEVICE, bdDevice);
@@ -788,6 +796,7 @@ final class RemoteDevices {
                             debugLog("BT_PROPERTY_ADV_AUDIO_ACTION_UUID SiZE"
                                 + tmpUuidArr.length +
                                 " Device uuid size " + device.mUuids.length);
+                            device.mAdvAudioUuids.clear();
                             if (!device.isBondingInitiatedLocally()) {
                               device.setBondingInitiatedLocally(true);
                             }
