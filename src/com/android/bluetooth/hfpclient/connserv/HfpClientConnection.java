64624b923325f76d0cd7754deb59ab5054252d7a
Srilakshmi Kanagalla <kanagall@codeaurora.org>
2021-05-03 11:58:17 +0530

    Changes for PTS HFP/HF/ACS/BI-13-I
    Change-Id: I3f8b40ad2cabc0c175b01d3f9aa423de90187b06

--- src/com/android/bluetooth/hfpclient/connserv/HfpClientConnection.java
+++ src/com/android/bluetooth/hfpclient/connserv/HfpClientConnection.java
@@ -25,7 +25,7 @@ import android.telecom.DisconnectCause;
 import android.telecom.PhoneAccount;
 import android.telecom.TelecomManager;
 import android.util.Log;
-
+import android.os.SystemProperties;
 import java.util.UUID;
 
 public class HfpClientConnection extends Connection {
@@ -80,8 +80,9 @@ public class HfpClientConnection extends Connection {
             Log.e(TAG, "Failed to create the call, dial failed.");
             return;
         }
-
-        mHeadsetProfile.connectAudio(device);
+        if(!SystemProperties.getBoolean("vendor.bt.pts.certification", false)){
+            mHeadsetProfile.connectAudio(mDevice);
+        }
         setInitializing();
         setDialing();
         finishInitializing();
@@ -278,7 +279,9 @@ public class HfpClientConnection extends Connection {
             mHeadsetProfile.acceptCall(mDevice, BluetoothHeadsetClient.CALL_ACCEPT_NONE);
         }
 
-        mHeadsetProfile.connectAudio(mDevice);
+        if(!SystemProperties.getBoolean("vendor.bt.pts.certification", false)){
+            mHeadsetProfile.connectAudio(mDevice);
+        }
     }
 
     @Override
