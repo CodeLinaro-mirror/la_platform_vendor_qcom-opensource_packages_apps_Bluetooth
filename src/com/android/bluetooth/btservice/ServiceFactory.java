993410aab075e89adc0906c292fca18f66e5c261
Srilakshmi kanagall <kanagall@codeaurora.org>
2021-05-31 05:54:47 -0700

    Revert "BLUETOOTH: Enable Auto connect for HFP CLient profile"
    This reverts commit 5150c4c7af8d3b8755e178922b46b4344ef02322.
    
    Change-Id: I186d1341f00dc16b5573939e3f83c7671fef12ff

--- src/com/android/bluetooth/btservice/ServiceFactory.java
+++ src/com/android/bluetooth/btservice/ServiceFactory.java
@@ -21,7 +21,6 @@ import com.android.bluetooth.a2dpsink.A2dpSinkService;
 import com.android.bluetooth.avrcp.AvrcpTargetService;
 import com.android.bluetooth.hearingaid.HearingAidService;
 import com.android.bluetooth.hfp.HeadsetService;
-import com.android.bluetooth.hfpclient.HeadsetClientService;
 import com.android.bluetooth.hid.HidDeviceService;
 import com.android.bluetooth.hid.HidHostService;
 import com.android.bluetooth.pan.PanService;
@@ -34,10 +33,6 @@ public class ServiceFactory {
 
     public HeadsetService getHeadsetService() {
         return HeadsetService.getHeadsetService();
-    }
-
-    public HeadsetClientService getHeadsetClientService() {
-        return HeadsetClientService.getHeadsetClientService();
     }
 
     public HidHostService getHidHostService() {
--- src/com/android/bluetooth/btservice/storage/Metadata.java
+++ src/com/android/bluetooth/btservice/storage/Metadata.java
@@ -49,7 +49,6 @@ class Metadata {
     public long last_active_time;
     public boolean is_active_a2dp_device;
     public boolean is_active_hfp_device;
-    public boolean is_active_hfpclient_device;
     public boolean is_connected_a2dpsrc_device;
 
     Metadata(String address) {
@@ -62,7 +61,6 @@ class Metadata {
         last_active_time = MetadataDatabase.sCurrentConnectionNumber++;
         is_active_a2dp_device = true;
         is_active_hfp_device = true;
-        is_active_hfpclient_device = true;
         is_connected_a2dpsrc_device = true;
     }
 
