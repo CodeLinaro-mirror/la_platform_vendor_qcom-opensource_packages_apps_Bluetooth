d009cdc686afd25a7dafbe54c694b1c40eb60c50
Yanfei Zhou <zyanfei@codeaurora.org>
2021-08-07 19:54:47 +0800

    AVRCP_CT: Expose API getElementAttribute through onCustomAction
    This feature is required by PTS test cases AVRCP/CT/CA.
    
    Change-Id: I48e3e733022eae8c80fafc5c7f00cbfc27f44243

--- jni/com_android_bluetooth_avrcp_controller.cpp
+++ jni/com_android_bluetooth_avrcp_controller.cpp
@@ -1436,6 +1436,44 @@ static void getItemAttributesNative(JNIEnv* env, jobject object, jbyteArray addr
   env->ReleaseByteArrayElements(address, addr, 0);
 }
 
+static void getElementAttributesNative(JNIEnv *env, jobject object, jbyteArray address,
+                                     jbyte numAttr, jintArray attrIds) {
+  if (!sBluetoothAvrcpInterface) return;
+
+  jbyte* addr = env->GetByteArrayElements(address, NULL);
+  if (!addr) {
+    jniThrowIOException(env, EINVAL);
+    return;
+  }
+
+  RawAddress rawAddress;
+  rawAddress.FromOctets((uint8_t*)addr);
+
+  if (numAttr > BTRC_MAX_ELEM_ATTR_SIZE) {
+    ALOGE("getElementAttributesNative: number of attributes exceed maximum");
+    return;
+  }
+
+  jint* attr = NULL;
+  if ((numAttr > 0) && (attrIds != NULL)) {
+    attr = env->GetIntArrayElements(attrIds, NULL);
+    if (!attr) {
+      jniThrowIOException(env, EINVAL);
+      return;
+    }
+  }
+
+  ALOGI("%s: sBluetoothAvrcpInterface: %p", __func__, sBluetoothAvrcpInterface);
+  bt_status_t status = sBluetoothAvrcpInterface->get_element_attribute_cmd(
+      rawAddress, numAttr, (uint32_t*)attr);
+  if (status != BT_STATUS_SUCCESS) {
+    ALOGE("Failed sending getElementAttributesNative command, status: %d", status);
+  }
+
+  if (attr) env->ReleaseIntArrayElements(attrIds, attr, 0);
+  env->ReleaseByteArrayElements(address, addr, 0);
+}
+
 static JNINativeMethod sMethods[] = {
     {"classInitNative", "()V", (void*)classInitNative},
     {"initNative", "()V", (void*)initNative},
@@ -1461,6 +1499,7 @@ static JNINativeMethod sMethods[] = {
     {"searchNative", "([BIILjava/lang/String;)V", (void*)searchNative},
     {"getSearchListNative", "([BII)V", (void*)getSearchListNative},
     {"getItemAttributesNative", "([BBJIB[I)V",(void *) getItemAttributesNative},
+    {"getElementAttributesNative", "([BB[I)V",(void *) getElementAttributesNative},
 };
 
 int register_com_android_bluetooth_avrcp_controller(JNIEnv* env) {
