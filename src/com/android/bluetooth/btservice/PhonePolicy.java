172a088699733f90f4d86c5e36bd26573fd04dc3
himta ram <hram@codeaurora.org>
2021-10-01 10:48:22 +0530

    btservice: add auto connection HFP Client support
    Added the support to auto connection for HFP Client only device.
    
    Change-Id: I21651d893456dd54c1f7be4b2a0ddd1649fe6f21
    
    CRs-Fixed: 3048600

--- src/com/android/bluetooth/btservice/PhonePolicy.java
+++ src/com/android/bluetooth/btservice/PhonePolicy.java
@@ -686,6 +686,7 @@ class PhonePolicy {
                autoConnectA2dpSink(mostRecentlyConnectedA2dpSrcDevice);
             }
             if (mostRecentlyActiveA2dpDevice == null &&
+                mostRecentlyActiveHfpClientDevice == null &&
                 mostRecentlyActiveHfpDevice == null) {
                 errorLog("autoConnect: most recently active a2dp and hfp devices are null");
                 return;
@@ -721,6 +722,10 @@ class PhonePolicy {
                     autoConnectHeadset(peerTwsDevice);
                     autoConnectHeadsetClient(mostRecentlyActiveHfpClientDevice);
                 }
+            } else if (mostRecentlyActiveHfpClientDevice != null) {
+                debugLog("autoConnect: recently connected HfpClientDevice " +
+                mostRecentlyActiveHfpClientDevice + " attempting auto connection HFP Client");
+                autoConnectHeadsetClient(mostRecentlyActiveHfpClientDevice);
             }
         } else {
             debugLog("autoConnect() - BT is in quiet mode. Not initiating auto connections");
