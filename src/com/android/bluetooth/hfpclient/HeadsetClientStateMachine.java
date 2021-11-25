951cf88bb4eba2500bcd14bd3f4404c22b29d4ed
Ying Nie <quic_yingnie@quicinc.com>
2021-11-25 19:44:51 +0530

    HeadsetClient: Ignore DISCONNECT_AUDIO message when device is in connected state
    Issue: In some cases HfpClientConnection Service is calling
           HeadsetClient's DISCONNECT_AUDIO after SCO disconnection.
           Due to this SCO state is moving to UNUSED and subsequent SCO
           request from Companion device is being rejected.
    
    Fix: Ignore DISCONNECT_AUDIO message when device's StateMachine is in
         connected state.
    
    CRs-Fixed: 3051391
    Change-Id: I9eb9f2095976c962e1c753bb4274a1629486944e

--- src/com/android/bluetooth/hfpclient/HeadsetClientStateMachine.java
+++ src/com/android/bluetooth/hfpclient/HeadsetClientStateMachine.java
@@ -1309,9 +1309,7 @@ public class HeadsetClientStateMachine extends StateMachine {
                     break;
 
                 case DISCONNECT_AUDIO:
-                    if (!mNativeInterface.disconnectAudio(getByteAddress(mCurrentDevice))) {
-                        Log.e(TAG, "ERROR: Couldn't disconnect Audio for device " + mCurrentDevice);
-                    }
+                    Log.e(TAG, "ERROR: Connected: ignore DISCONNECT_AUDIO, device= " + mCurrentDevice);
                     break;
 
                 case VOICE_RECOGNITION_START:
@@ -1720,6 +1718,7 @@ public class HeadsetClientStateMachine extends StateMachine {
                             SCO_REJECT_DELAY_MS);
                         // Don't continue connecting!
                         Log.d(TAG, "Disconnecting Audio after " + SCO_REJECT_DELAY_MS + " ms as routing not allowed");
+                        transitionTo(mAudioOn);
                         return;
                     }
                     // Audio state is split in two parts, the audio focus is maintained by the
