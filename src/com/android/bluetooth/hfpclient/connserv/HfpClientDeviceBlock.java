3a768816b09ac5e9bd357ff88ac03daa90cd5030
himta ram <quic_hram@quicinc.com>
2022-05-26 16:22:02 +0530

    connserv: use HeadsetClient profile name for hfp client service
      - Use HeadsetClient profile name for hfp client service intraction.
    
    Change-Id: I7ad62130d52af53177b38baf600aebb029a67285

--- src/com/android/bluetooth/hfpclient/connserv/HfpClientDeviceBlock.java
+++ src/com/android/bluetooth/hfpclient/connserv/HfpClientDeviceBlock.java
@@ -51,7 +51,7 @@ public class HfpClientDeviceBlock {
     private final HfpClientConnectionService mConnServ;
     private HfpClientConference mConference;
 
-    private BluetoothHeadsetClient mHeadsetProfile;
+    private BluetoothHeadsetClient mHeadsetClientProfile;
 
     HfpClientDeviceBlock(HfpClientConnectionService connServ, BluetoothDevice device,
             BluetoothHeadsetClient headsetProfile) {
@@ -69,11 +69,11 @@ public class HfpClientDeviceBlock {
         mTelecomManager.registerPhoneAccount(mPhoneAccount);
         mTelecomManager.enablePhoneAccount(mPhoneAccount.getAccountHandle(), true);
         mTelecomManager.setUserSelectedOutgoingPhoneAccount(mPhoneAccount.getAccountHandle());
-        mHeadsetProfile = headsetProfile;
+        mHeadsetClientProfile = headsetProfile;
 
         // Read the current calls and add them to telecom if already present
-        if (mHeadsetProfile != null) {
-            List<BluetoothHeadsetClientCall> calls = mHeadsetProfile.getCurrentCalls(mDevice);
+        if (mHeadsetClientProfile != null) {
+            List<BluetoothHeadsetClientCall> calls = mHeadsetClientProfile.getCurrentCalls(mDevice);
             if (DBG) {
                 Log.d(mTAG, "Got calls " + calls);
             }
@@ -130,7 +130,7 @@ public class HfpClientDeviceBlock {
     synchronized void onConference(Connection connection1, Connection connection2) {
         if (mConference == null) {
             mConference = new HfpClientConference(mPhoneAccount.getAccountHandle(), mDevice,
-                    mHeadsetProfile);
+                    mHeadsetClientProfile);
         }
 
         if (connection1.getConference() == null) {
@@ -203,10 +203,10 @@ public class HfpClientDeviceBlock {
             // telecom, it would be unclear which call would take priority for audio.
             if (mConnections.isEmpty() && mTelecomManager.isInCall()
                     && call.getState() != BluetoothHeadsetClientCall.CALL_STATE_INCOMING) {
-                if (mHeadsetProfile != null && mHeadsetProfile.getAudioState(mDevice) ==
+                if (mHeadsetClientProfile != null && mHeadsetClientProfile.getAudioState(mDevice) ==
                     BluetoothHeadsetClient.STATE_AUDIO_CONNECTED) {
                     Log.d(mTAG, " CS call in progress, disconnect SCO for Client call");
-                    mHeadsetProfile.disconnectAudio(mDevice);
+                    mHeadsetClientProfile.disconnectAudio(mDevice);
                 }
                 return;
             }
@@ -293,7 +293,7 @@ public class HfpClientDeviceBlock {
 
     private synchronized HfpClientConnection buildConnection(BluetoothHeadsetClientCall call,
             Uri number) {
-        if (mHeadsetProfile == null) {
+        if (mHeadsetClientProfile == null) {
             Log.e(mTAG,
                     "Cannot create connection for call " + call + " when Profile not available");
             return null;
@@ -310,9 +310,9 @@ public class HfpClientDeviceBlock {
 
         HfpClientConnection connection = null;
         if (call != null) {
-            connection = new HfpClientConnection(mConnServ, mDevice, mHeadsetProfile, call);
+            connection = new HfpClientConnection(mConnServ, mDevice, mHeadsetClientProfile, call);
         } else {
-            connection = new HfpClientConnection(mConnServ, mDevice, mHeadsetProfile, number);
+            connection = new HfpClientConnection(mConnServ, mDevice, mHeadsetClientProfile, number);
         }
 
         if (connection.getState() != Connection.STATE_DISCONNECTED) {
@@ -351,7 +351,7 @@ public class HfpClientDeviceBlock {
                 // If this is the first connection with conference, create the conference first.
                 if (mConference == null) {
                     mConference = new HfpClientConference(mPhoneAccount.getAccountHandle(), mDevice,
-                            mHeadsetProfile);
+                            mHeadsetClientProfile);
                 }
                 if (mConference.addConnection(otherConn)) {
                     if (DBG) {
@@ -397,7 +397,7 @@ public class HfpClientDeviceBlock {
 
         Log.d(mTAG, "enableAudio: setAllowed= " + setAllowed + " reRoute= " + reRoute);
 
-        if (mHeadsetProfile == null) {
+        if (mHeadsetClientProfile == null) {
             Log.e(mTAG, "Unable to enableAudio: Headset profile not ready. ");
             return;
         }
@@ -408,8 +408,8 @@ public class HfpClientDeviceBlock {
             return;
         }
 
-        if (mHeadsetProfile.getAudioRouteAllowed(mDevice) != setAllowed) {
-            mHeadsetProfile.setAudioRouteAllowed(mDevice, setAllowed);
+        if (mHeadsetClientProfile.getAudioRouteAllowed(mDevice) != setAllowed) {
+            mHeadsetClientProfile.setAudioRouteAllowed(mDevice, setAllowed);
 
             HeadsetService headsetService = HeadsetService.getHeadsetService();
             if (headsetService != null) {
@@ -420,9 +420,9 @@ public class HfpClientDeviceBlock {
         // app takes care of redundant calls.
         if (reRoute) {
             if (setAllowed) {
-                mHeadsetProfile.connectAudio(mDevice);
+                mHeadsetClientProfile.connectAudio(mDevice);
             } else {
-                mHeadsetProfile.disconnectAudio(mDevice);
+                mHeadsetClientProfile.disconnectAudio(mDevice);
             }
         }
     }
