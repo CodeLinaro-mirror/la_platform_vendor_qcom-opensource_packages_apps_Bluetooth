c92dfd77ebf60d9631877578c13948a08a835fd4
Santosh Rotti <quic_srotti@quicinc.com>
2022-07-08 17:39:17 +0530

    A2DP sink: Null pointer Exception in inCallFromstreamingDevice
     Added check in A2DP sink stream handler to avoid the null pointer
     exception if current calls are none.
    
    Change-Id: I41377456a932f997439af2129f0eb730f55c20db

--- src/com/android/bluetooth/a2dpsink/A2dpSinkStreamHandler.java
+++ src/com/android/bluetooth/a2dpsink/A2dpSinkStreamHandler.java
@@ -391,8 +391,9 @@ public class A2dpSinkStreamHandler extends Handler {
             targetDevice = connectedDevices.get(0);
         }
         HeadsetClientService headsetClientService = HeadsetClientService.getHeadsetClientService();
-        if (targetDevice != null && headsetClientService != null) {
-            return headsetClientService.getCurrentCalls(targetDevice).size() > 0;
+        if (targetDevice != null && headsetClientService != null &&
+                        headsetClientService.getCurrentCalls(targetDevice) != null) {
+                return headsetClientService.getCurrentCalls(targetDevice).size() > 0;
         }
         return false;
     }
