3aa9467a07339657d19946d49822dcd7dce47c49
Fei Zheng <quic_zfei@quicinc.com>
2022-08-24 17:59:35 +0800

    AVRCP_CT: Fix changing phone volume failure when to adjust on DUT
    Issue:
    When to adjust media volume on DUT, phone media volume cannot be changed
    
    Root cause:
    When to handle Set Absolute Volume command or Register Absolute Volume
    Notification, transaction label in response is not set to be same with command
    
    Fix:
    Set transaction label in response to be same with command
    
    Change-Id: Idbc49a47e62f20b3cf209ea4ba869b16e09b96c0

--- src/com/android/bluetooth/avrcpcontroller/AvrcpControllerService.java
+++ src/com/android/bluetooth/avrcpcontroller/AvrcpControllerService.java
@@ -457,7 +457,7 @@ public class AvrcpControllerService extends ProfileService {
         AvrcpControllerStateMachine stateMachine = getStateMachine(device);
         if (stateMachine != null) {
             stateMachine.sendMessage(
-                    AvrcpControllerStateMachine.MESSAGE_PROCESS_REGISTER_ABS_VOL_NOTIFICATION);
+                    AvrcpControllerStateMachine.MESSAGE_PROCESS_REGISTER_ABS_VOL_NOTIFICATION, label);
         }
     }
 
@@ -470,7 +470,7 @@ public class AvrcpControllerService extends ProfileService {
         AvrcpControllerStateMachine stateMachine = getStateMachine(device);
         if (stateMachine != null) {
             stateMachine.sendMessage(AvrcpControllerStateMachine.MESSAGE_PROCESS_SET_ABS_VOL_CMD,
-                    absVol);
+                    absVol, label);
         }
     }
 
