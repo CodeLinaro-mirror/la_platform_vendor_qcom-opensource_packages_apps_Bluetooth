46af35f262845e38306508101e3292538d02b2d2
Amit Kumar Prasad <quic_amitpras@quicinc.com>
2022-10-21 13:35:01 +0530

    A2dp SNK: Issue Avrcp Pause only when streaming
    -Check is added before sending Avrcp Pause to 
     avoid duplicate pause command.
    
    -Pause will be issued only for the first time.
    
    CRs-Fixed: 3292440
    Change-Id: Ie0623b57f74dd37003d2a1a2ab6e1d80f8a5c8f7

--- src/com/android/bluetooth/a2dpsink/A2dpSinkService.java
+++ src/com/android/bluetooth/a2dpsink/A2dpSinkService.java
@@ -398,6 +398,7 @@ public class A2dpSinkService extends ProfileService {
                 mA2dpSinkStreamHandler.obtainMessage(
                     A2dpSinkStreamHandler.SRC_PAUSE).sendToTarget();
             } else {
+                mStateMachine.setMediaControl(A2dpSinkStateMachine.MEDIA_CONTROL_NONE);
                 // Soft-Handoff from AVRCP Cmd (if received before AVDTP_START)
                 initiateHandoffOperations(device);
                 if (mStreamingDevice != null && !mStreamingDevice.equals(device)) {
@@ -427,12 +428,17 @@ public class A2dpSinkService extends ProfileService {
                    // Send Passthrough Command for PAUSE
                    AvrcpControllerService avrcpService =
                            AvrcpControllerService.getAvrcpControllerService();
-                  avrcpService.sendPassThroughCommandNative(Utils.getByteAddress(mStreamingDevice),
-                              AvrcpControllerService.PASS_THRU_CMD_ID_PAUSE,
-                               AvrcpControllerService.KEY_STATE_PRESSED);
-                  avrcpService.sendPassThroughCommandNative(Utils.getByteAddress(mStreamingDevice),
-                              AvrcpControllerService.PASS_THRU_CMD_ID_PAUSE,
-                                AvrcpControllerService.KEY_STATE_RELEASED);
+                   if(otherSm.getMediaControl() == A2dpSinkStateMachine.MEDIA_CONTROL_NONE) {
+                      otherSm.setMediaControl(A2dpSinkStateMachine.MEDIA_CONTROL_PAUSE);
+                      avrcpService.sendPassThroughCommandNative(Utils.getByteAddress(mStreamingDevice),
+                                  AvrcpControllerService.PASS_THRU_CMD_ID_PAUSE,
+                                   AvrcpControllerService.KEY_STATE_PRESSED);
+                      avrcpService.sendPassThroughCommandNative(Utils.getByteAddress(mStreamingDevice),
+                                  AvrcpControllerService.PASS_THRU_CMD_ID_PAUSE,
+                                   AvrcpControllerService.KEY_STATE_RELEASED);
+                   } else {
+                      Log.d(TAG, "skip sending redundant pause for " + otherDevice);
+                   }
                    /* set autoconnect priority of non-streaming device to PRIORITY_ON and priority
                     *  of streaming device to PRIORITY_AUTO_CONNECT */
                    avrcpService.onDeviceUpdated(device);
