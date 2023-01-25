c8e5c99489fb222cec1ffaef43b35c29184ae48e
Amit Kumar Prasad <quic_amitpras@quicinc.com>
2023-01-25 17:39:23 +0530

    BluetoothApp: set and get afh support
    CRs-Fixed: 3396190
    Change-Id: I10c02f27084e299d8388097d0dbc6fa60172ec5d

--- src/com/android/bluetooth/btservice/AdapterService.java
+++ src/com/android/bluetooth/btservice/AdapterService.java
@@ -1251,6 +1251,10 @@ public class AdapterService extends Service {
         public boolean isLeHighPriorityModeSet(BluetoothDevice device,
                 AttributionSource attributionSource) { return false; }
 
+        public boolean setAfhChannelMap(int transport, int len, byte [] afhMap,
+                AttributionSource attributionSource) { return false; }
+        public boolean getAfhChannelMap(BluetoothDevice device, int transport,
+                AttributionSource attributionSource) { return false; }
         @Override
         public int getDeviceType(BluetoothDevice device, AttributionSource source)
             { return TYPE_BREDR; }
