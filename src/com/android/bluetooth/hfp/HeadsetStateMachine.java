9d7c08f80190c9a95d46f342c66505859505671e
Song Gao <quic_songg@quicinc.com>
2023-03-08 14:06:37 +0800

    Headset: Add missing markings
    Change-Id: I86d08058bd6d13b4e5c2b1ed2b040ab06d31e2de

--- src/com/android/bluetooth/hfp/HeadsetStateMachine.java
+++ src/com/android/bluetooth/hfp/HeadsetStateMachine.java
@@ -14,6 +14,13 @@
  * limitations under the License.
  */
 
+/*
+ * Changes from Qualcomm Innovation Center are provided under the following license:
+ *
+ * Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause-Clear
+ */
+
 package com.android.bluetooth.hfp;
 
 import static android.Manifest.permission.BLUETOOTH_CONNECT;
