fa15a353c74f2ffb69c2e8cbf66b63f93802657d
Song Gao <quic_songg@quicinc.com>
2023-05-22 10:55:10 +0800

    Bluetooth: Solve garbled characters issue in parsing adapter index
    Issue found:
    In monkey test, both Bluetooth adapters attempt to bind with default
    Bluetooth HIDL instance, therefore results in fatal error in Bluetooth
    process.
    
    Root cause:
    In current implementation of Bluetooth AdapterService, adapter index is
    converted to a string as part of init parameters by calling String.format().
    String.format() takes local default system language as language environment
    if caller does not explicitly indicate which language should be used in the
    parameter. However, during the monkey test, the system language is changed
    from US to Persian by accident, which results in the number '0' and '1' been
    changed to '۰' and '۱' unexpectedly. This causes failure in parsing adapter
    index correctly before binding relevant Bluetooth HIDL service.
    
    Resolution:
    Explicitly indicate the language to be used when calling String.format() to
    convert a integer to a string.
    
    Change-Id: I832bb8d8c76a89e5fe0bdd9cc24e9e127f921bed

--- src/com/android/bluetooth/btservice/AdapterService.java
+++ src/com/android/bluetooth/btservice/AdapterService.java
@@ -19,6 +19,43 @@
  * SPDX-License-Identifier: BSD-3-Clause-Clear.
  */
 
+/************************************************************************************
+ * Changes from Qualcomm Innovation Center are provided under the following license:
+ *
+ * Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted (subject to the limitations in the
+ * disclaimer below) provided that the following conditions are met:
+ *
+ *      * Redistributions of source code must retain the above copyright
+ *        notice, this list of conditions and the following disclaimer.
+ *
+ *      * Redistributions in binary form must reproduce the above
+ *        copyright notice, this list of conditions and the following
+ *        disclaimer in the documentation and/or other materials provided
+ *        with the distribution.
+ *
+ *      * Neither the name of Qualcomm Innovation Center, Inc. nor the names of its
+ *        contributors may be used to endorse or promote products derived
+ *        from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
+ * GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
+ * HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE
+ *
+ **********************************************************************************/
+
 package com.android.bluetooth.btservice;
 
 import static android.text.format.DateUtils.MINUTE_IN_MILLIS;
@@ -146,6 +183,7 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
 import java.util.function.Predicate;
@@ -3871,7 +3909,7 @@ public class AdapterService extends Service {
         if (DeviceConfig.getBoolean(DeviceConfig.NAMESPACE_BLUETOOTH, BTAA_HCI_LOG_FLAG, false)) {
             initFlags.add(String.format("%s=%s", BTAA_HCI_LOG_FLAG, "true"));
         }
-        initFlags.add(String.format("%s=%d", BT_HCI_ADAPTER_FLAG, mAdapterIndex));
+        initFlags.add(String.format(Locale.US, "%s=%d", BT_HCI_ADAPTER_FLAG, mAdapterIndex));
         return initFlags.toArray(new String[0]);
     }
 
