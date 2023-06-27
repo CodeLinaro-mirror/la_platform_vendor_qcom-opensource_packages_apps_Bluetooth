4baf78be16cb484a68fe96cdb33ad7aff7d65066
Rahul Samana <quic_samana@quicinc.com>
2023-06-27 16:18:24 +0530

    [SS] Headset failed to reconnect after disconnection.   - Page Scan is not enabled after ACL Disconnection Event.   - Sending transport type as intent extra to be used     in Application for enabling Page Scan
    Change-Id: Ifef122b30ebc26fe8404567765af99f22ffec4e7

--- src/com/android/bluetooth/btservice/RemoteDevices.java
+++ src/com/android/bluetooth/btservice/RemoteDevices.java
@@ -1080,6 +1080,7 @@ final class RemoteDevices {
         if (newState == AbstractionLayer.BT_ACL_STATE_CONNECTED) {
             if (state == BluetoothAdapter.STATE_ON || state == BluetoothAdapter.STATE_TURNING_ON) {
                 intent = new Intent(BluetoothDevice.ACTION_ACL_CONNECTED);
+                intent.putExtra(BluetoothDevice.EXTRA_TRANSPORT, transportLinkType);
             } else if (state == BluetoothAdapter.STATE_BLE_ON
                     || state == BluetoothAdapter.STATE_BLE_TURNING_ON) {
                 intent = new Intent(BluetoothAdapter.ACTION_BLE_ACL_CONNECTED);
@@ -1090,6 +1091,7 @@ final class RemoteDevices {
         } else {
             if (state == BluetoothAdapter.STATE_ON || state == BluetoothAdapter.STATE_TURNING_OFF) {
                 intent = new Intent(BluetoothDevice.ACTION_ACL_DISCONNECTED);
+                intent.putExtra(BluetoothDevice.EXTRA_TRANSPORT, transportLinkType);
             } else if (state == BluetoothAdapter.STATE_BLE_ON
                     || state == BluetoothAdapter.STATE_BLE_TURNING_OFF) {
                 intent = new Intent(BluetoothAdapter.ACTION_BLE_ACL_DISCONNECTED);
