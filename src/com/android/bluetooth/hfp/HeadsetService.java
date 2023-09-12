b8325f25fa51c249472b5f185a27cf1102944916
Haseena Shaik <quic_haseshai@quicinc.com>
2023-09-12 12:04:19 +0530

    Commenting action_roaming_state_change code temporarily
    -As RECEIVER_EXPORTED or RECEIVER_NOT_EXPORTED flag is not set
    
    Change-Id: I5437cecfb5152be72c0573281cb3ee17b7cecb8d
    
    CRs-Fixed: 3613419
    (cherry picked from commit 0a9816cd87f7dcc9ae678b19e4754def96022032)
    
    Change-Id: Ie0a2a44ef15be29e99422d27cb309d9cdfb15915

--- src/com/android/bluetooth/hfp/HeadsetService.java
+++ src/com/android/bluetooth/hfp/HeadsetService.java
@@ -190,7 +190,7 @@ public class HeadsetService extends ProfileService {
     private Context mContext = null;
     private AudioServerStateCallback mServerStateCallback = new AudioServerStateCallback();
     private static final int AUDIO_CONNECTION_DELAY_DEFAULT = 100;
-    private static final String ACTION_ROAMING_STATE_CHANGED = "android.bluetooth.action.ROAMING_STATE_CHANGED";
+   //private static final String ACTION_ROAMING_STATE_CHANGED = "android.bluetooth.action.ROAMING_STATE_CHANGED";
     @Override
     public IProfileServiceBinder initBinder() {
         return new BluetoothHeadsetBinder(this);
@@ -276,7 +276,7 @@ public class HeadsetService extends ProfileService {
         filter.addAction(BluetoothDevice.ACTION_BOND_STATE_CHANGED);
         filter.addAction(BluetoothA2dp.ACTION_PLAYING_STATE_CHANGED);
         filter.addAction(BluetoothA2dp.ACTION_CONNECTION_STATE_CHANGED);
-        filter.addAction(ACTION_ROAMING_STATE_CHANGED);
+        //filter.addAction(ACTION_ROAMING_STATE_CHANGED);
         registerReceiver(mHeadsetReceiver, filter);
         // Step 7: Mark service as started
 
@@ -616,13 +616,13 @@ public class HeadsetService extends ProfileService {
                     mHfpA2dpSyncInterface.updateA2DPConnectionState(intent);
                     break;
                 }
-                case ACTION_ROAMING_STATE_CHANGED: {
+                /*case ACTION_ROAMING_STATE_CHANGED: {
                     Log.e(TAG, "HeadsetService -  Received ACTION_ROAMING_STATE_CHANGED ");
                     synchronized (mStateMachines) {
                         doForEachConnectedStateMachine(stateMachine -> stateMachine.sendMessage(HeadsetStateMachine.UPDATE_ROAMING_STATE, intent));
                      }
                     break;
-                }
+                }*/
                 default:
                     Log.w(TAG, "Unknown action " + action);
             }
