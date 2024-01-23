65c01078d73e554621ad28a7b2c1b10b4946dc5c
Tyagaraju Gowda <quic_tgowda@quicinc.com>
2024-01-23 17:55:35 +0530

    Changes to enable Client roles
    - Added changes to connect PBAP_CLIENT and MAP_CLIENT during pairing.
      setting policy allowed for client roles, this will help to connect
    this profiles during pairing automatically
    
    Change-Id: Iba24158c74199d71a089c1a3cf8636bd52801855

--- src/com/android/bluetooth/mapclient/MapClientService.java
+++ src/com/android/bluetooth/mapclient/MapClientService.java
@@ -12,6 +12,10 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * Changes from Qualcomm Innovation Center, Inc. are provided under the following license:
+ * Copyright (c) 2024 Qualcomm Innovation Center, Inc. All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause-Clear
  */
 
 package com.android.bluetooth.mapclient;
@@ -458,7 +462,7 @@ public class MapClientService extends ProfileService {
         @RequiresPermission(android.Manifest.permission.BLUETOOTH_CONNECT)
         private MapClientService getService(AttributionSource source) {
             if (!Utils.checkServiceAvailable(mService, TAG)
-                    || Utils.checkCallerIsSystemOrActiveOrManagedUser(mService, TAG)
+                    || !Utils.checkCallerIsSystemOrActiveOrManagedUser(mService, TAG)
                     || !Utils.checkConnectPermissionForDataDelivery(mService, source, TAG)) {
                 return null;
             }
