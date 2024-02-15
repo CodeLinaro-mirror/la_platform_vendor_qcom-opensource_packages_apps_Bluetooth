c90f5956bc690fec98910768ae3b9c630a83f8a0
Santosh Rotti <quic_srotti@quicinc.com>
2024-02-15 11:51:09 +0530

    A2DP : Add changes to avoid newly connected device to become active.
     - Added code changes to avoid newly connected device to become active device.
    
    Change-Id: I06386eba3d4a4dfd7e3008de0978ce223e786185

--- src/com/android/bluetooth/btservice/ActiveDeviceManager.java
+++ src/com/android/bluetooth/btservice/ActiveDeviceManager.java
@@ -386,7 +386,10 @@ public class ActiveDeviceManager {
                         mA2dpConnectedDevices.add(device);
                         if (mHearingAidActiveDevice == null) {
                             // New connected device: select it as active
-                            setA2dpActiveDevice(device);
+                            if (mA2dpConnectedDevices.size() <= 1) {
+                                setA2dpActiveDevice(device);
+                                Log.d(TAG, "Set active device for first device");
+                            }
                             break;
                         } else {
                             if (!ApmConstIntf.getQtiLeAudioEnabled()) {
