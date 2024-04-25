cdf3d8b084bc5bf15ce7482209a889e070b27c10
Rahul Samana <quic_samana@quicinc.com>
2024-04-25 16:00:29 +0530

    Adding support for Dynamic Audio Buffer Support
    Change-Id: Ic28db2686ea3f8b84e003b5b98697a462a32f0e4
    (cherry picked from commit c0e48aea0288bb6d07c6ef6870a10bffa77aba69)

--- src/com/android/bluetooth/btservice/AdapterService.java
+++ src/com/android/bluetooth/btservice/AdapterService.java
@@ -611,7 +611,7 @@ public class AdapterService extends Service {
                         initProfileServices();
                         //getAdapterPropertyNative(AbstractionLayer.BT_PROPERTY_LOCAL_IO_CAPS);
                         //getAdapterPropertyNative(AbstractionLayer.BT_PROPERTY_RESERVED_0F);
-                        //getAdapterPropertyNative(AbstractionLayer.BT_PROPERTY_DYNAMIC_AUDIO_BUFFER);
+                        getAdapterPropertyNative(AbstractionLayer.BT_PROPERTY_DYNAMIC_AUDIO_BUFFER);
                         mAdapterStateMachine.sendMessage(AdapterState.BREDR_STARTED);
                         mBtCompanionManager.loadCompanionInfo();
                         //update wifi state to lower layers
