3a8f94e4faac9a4604d6e17333a51f06430da9c9
Srinu Jella <quic_sjella@quicinc.com>
2024-07-23 23:34:56 +0530

    HFP client: Issue the sco disconnection only if SCO is active
    - Issue SCO disconnection only if SCO state is connecting or
      connected.
    
    CRs-Fixed: 3876969
    Change-Id: I663442296d3b0fe656ca1db45715c3eda1f7e6b9

--- src/com/android/bluetooth/hfpclient/HeadsetClientStateMachine.java
+++ src/com/android/bluetooth/hfpclient/HeadsetClientStateMachine.java
@@ -1202,8 +1202,10 @@ public class HeadsetClientStateMachine extends StateMachine {
 
                 case DISCONNECT_AUDIO:
                     removeMessages(AUDIO_CONNECTING_TIMEOUT);
-                    if (!mNativeInterface.disconnectAudio(getByteAddress(mCurrentDevice))) {
-                        Log.e(TAG, "ERROR: Couldn't disconnect Audio for device " + mCurrentDevice);
+                    if(mAudioState == BluetoothHeadsetClient.STATE_AUDIO_CONNECTING) {
+                        if (!mNativeInterface.disconnectAudio(getByteAddress(mCurrentDevice))) {
+                            Log.e(TAG, "ERROR: Couldn't disconnect Audio for device " + mCurrentDevice);
+                        }
                     }
                     break;
 
