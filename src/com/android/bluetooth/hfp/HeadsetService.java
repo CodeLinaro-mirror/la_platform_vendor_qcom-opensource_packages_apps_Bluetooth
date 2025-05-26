de9c611d6d4a64f426f3569b9446d9f8dbeffaa2
Tyagaraju Gowda <tgowda@qti.qualcomm.com>
2025-05-26 18:10:08 +0530

    HFP_AG: Fix: Incoming call not routing to headset
    --When we are in source role incoming call not routing to headset in hf
    gateway role. We observed that in source role even though le audio
    disabled it used to send commands to le audio module hence it's failing.
    Now we added check and routing into headset module during le audio
    disabled.
    
    Change-Id: I7431ea4faa47a88090772585a5a0af24715db57b

--- src/com/android/bluetooth/hfp/HeadsetService.java
+++ src/com/android/bluetooth/hfp/HeadsetService.java
@@ -12,40 +12,11 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */
-/* Changes from Qualcomm Innovation Center are provided under the following license:
  *
- * Copyright (c) 2021 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Changes from Qualcomm Technologies, Inc. are provided under the following license:
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ * SPDX-License-Identifier: BSD-3-Clause-Clear
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted (subject to the limitations in the
- * disclaimer below) provided that the following conditions are met:
- *
- *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *
- *     * Redistributions in binary form must reproduce the above
- *       copyright notice, this list of conditions and the following
- *       disclaimer in the documentation and/or other materials provided
- *       with the distribution.
- *
- *     * Neither the name of Qualcomm Innovation Center, Inc. nor the names of its
- *       contributors may be used to endorse or promote products derived
- *       from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
- * GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
- * HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
- * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
- * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
- * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
- * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
- * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
- * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE
  */
 
 package com.android.bluetooth.hfp;
@@ -1009,6 +980,13 @@ public class HeadsetService extends ProfileService {
                 Log.d(TAG, "Adv Audio enabled: phoneStateChanged");
                 CallControlIntf mCallControl = CallControlIntf.get();
                 mCallControl.phoneStateChanged(numActive, numHeld, callState, number, type, name, false);
+            } else {
+                HeadsetService service = getService(source);
+                if (service == null) {
+                    Log.e(TAG, "HeadsetService is null");
+                    return;
+                }
+                service.phoneStateChanged(numActive, numHeld, callState, number, type, name, false);
             }
         }
 
@@ -1020,6 +998,13 @@ public class HeadsetService extends ProfileService {
                Log.d(TAG, "Adv Audio enabled: clccResponse");
                CallControlIntf mCallControl = CallControlIntf.get();
                mCallControl.clccResponse(index, direction, status, mode, mpty, number, type);
+            } else {
+                HeadsetService service = getService(source);
+                if (service == null) {
+                    Log.e(TAG, "HeadsetService is null");
+                    return;
+                }
+                service.clccResponse(index, direction, status, mode, mpty, number, type);
             }
         }
 
