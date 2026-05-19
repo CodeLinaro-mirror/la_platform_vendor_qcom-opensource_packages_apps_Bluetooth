88f1c120228d25df5cc1843aedc7d24e6de2998e
Sreerag Vaniyam Veettil <sveettil@qti.qualcomm.com>
2026-05-19 02:04:22 -0700

    BT_GATT: Initiate GATT Disconnection as part of BREDR Disconnection
    Change-Id: I841a853d32397826074ff6db173db21030a7ac42

--- src/com/android/bluetooth/gatt/GattService.java
+++ src/com/android/bluetooth/gatt/GattService.java
@@ -16,6 +16,11 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * Changes from Qualcomm Technologies, Inc. are provided under the following license:
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ * SPDX-License-Identifier: BSD-3-Clause-Clear
+ *
  */
 
 package com.android.bluetooth.gatt;
@@ -2721,6 +2726,10 @@ public class GattService extends ProfileService {
 
     public void setAptXLowLatencyMode(boolean enabled){
         mScanManager.setAptXLowLatencyMode(enabled);
+    }
+
+    public void serverDisconnectIf(int serverIf, String address, AttributionSource attributionSource) {
+        serverDisconnect(serverIf, address, attributionSource);
     }
 
     /**************************************************************************
